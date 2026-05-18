e1984525c72f9583eb433cb119c576e6ee089b98
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-17 22:30:03 -0300

    Instalando o pacote day.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
+// Configuracao do dayjs
+import './libs/dayjs.js';
+
 // Css
 import './styles/global.css';
 import './styles/form.css';
 import './styles/schedule.css';
+
+import dayjs from 'dayjs';
+console.log(dayjs().format('HH:mm'));
