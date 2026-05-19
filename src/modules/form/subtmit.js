33206802f076442308326b9b92a25797d9231028
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-19 20:48:28 -0300

    Atualizando a lista de agendamentos

--- src/modules/form/subtmit.js
+++ src/modules/form/subtmit.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 
 import { scheduleNew } from '../../services/schedule-new.js';
+import { schedulesDay } from '../schedules/load.js';
 
 const form = document.querySelector('form');
 const clientName = document.getElementById('client');
@@ -43,6 +44,10 @@ form.onsubmit = async (event) => {
             name,
             when,
         });
+
+        //  Recarrega os agendamentos
+        await schedulesDay();
+        clientName.value = '';
     } catch (error) {
         alert('Não foi possível realizar o agendamento.');
         console.log(error);
