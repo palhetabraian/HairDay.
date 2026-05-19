1f654eaca004ca0ee37e8d8ce499863190dde944
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-19 06:16:41 -0300

    Selecionando um horário

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 
 import { openingHours } from '../../utils/opening-hours.js';
+import { hoursClick } from './hours-click.js';
 
 const hours = document.getElementById('hours');
 
@@ -37,6 +38,9 @@ export function hoursLoad({ date }) {
 
         hours.append(li);
     });
+
+    //Adiciona o evento de clique nos horarios disponiveis
+    hoursClick();
 }
 
 function hourHeaderAdd(title) {
