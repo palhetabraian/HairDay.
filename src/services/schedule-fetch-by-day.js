3edaa31cbbaaa35241fae2aad04d2c01358c0cbd
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-19 08:56:47 -0300

    Serviço para buscar agendamentos do dia

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs';
+import { apiConfig } from './api-config';
+
+export async function scheduleFetchByDay({ date }) {
+    try {
+        // Fazendo a requisicao.
+        const response = await fetch(`${apiConfig.baseURL}/schedules`);
+
+        // Converte para JSON
+        const data = await response.json();
+
+        //Filtra os agendamentos pelo dia selecionado.
+        const dailySchedules = data.filter((schedule) => dayjs(date).isSame(schedule.when, 'day'));
+    } catch (error) {
+        console.log(error);
+        alert('Não foi possível buscar os agendamentos do dia selecionado');
+    }
+}
--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,13 +2,15 @@ import { apiConfig } from './api-config.js';
 
 export async function scheduleNew({ id, name, when }) {
     try {
+        // faz a requisicao para enviar os dados do agendamento
         await fetch(`${apiConfig.baseURL}/schedules`, {
-            method: 'POST',
+            methodes: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ id, name, when }),
         });
+        // Exibe mensagem de agendamento realizado.
         alert('Agendamento realizado com sucesso!');
     } catch (error) {
         console.log(error);
