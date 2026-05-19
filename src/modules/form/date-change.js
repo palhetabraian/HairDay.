9534e1b9ea5fbaccb44722c1c1db5f9bcfa1a801
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-19 08:07:18 -0300

    Carregando horários para outros dias

---
+++ src/modules/form/date-change.js
@@ -0,0 +1,8 @@
+import { schedulesDay } from '../schedules/load';
+
+// Seleciona o input de data
+const selectedDate = document.getElementById('date');
+
+// Recarrega a lista de horarios quando o input de data mudar.
+
+selectedDate.onchange = () => schedulesDay();
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -3,7 +3,7 @@ import { hoursLoad } from '../form/hours-load.js';
 // Seleciona o input de data.
 const selectedDate = document.getElementById('date');
 
-export function scheduleDays() {
+export function schedulesDay() {
     //Obtem a data do input
     const date = selectedDate.value;
 
