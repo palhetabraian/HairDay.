43848833b2dc2f39812276c80ddce597aa609d71
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-19 06:09:02 -0300

    Separando os horarios pelo periodo

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -15,7 +15,7 @@ export function hoursLoad({ date }) {
         // Define se o horario esta disponivel
         return {
             hour,
-            available: !isHourPast,
+            available: isHourPast,
         };
     });
 
@@ -26,6 +26,23 @@ export function hoursLoad({ date }) {
         li.classList.add(available ? 'hour-available' : 'hour-unavailable');
 
         li.textContent = hour;
+
+        if (hour === '9:00') {
+            hourHeaderAdd('Manhã');
+        } else if (hour === '13:00') {
+            hourHeaderAdd('Tarde');
+        } else if (hour === '18:00') {
+            hourHeaderAdd('Noite');
+        }
+
         hours.append(li);
     });
+}
+
+function hourHeaderAdd(title) {
+    const header = document.createElement('li');
+    header.classList.add('hour-period');
+    header.textContent = title;
+
+    hours.append(header);
 }
