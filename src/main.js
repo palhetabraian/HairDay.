4831ad927735a392a4e9d88e6b405265f260232e
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-19 05:24:29 -0300

    Definindo a data atual

--- src/main.js
+++ src/main.js
@@ -8,5 +8,5 @@ import './styles/global.css';
 import './styles/form.css';
 import './styles/schedule.css';
 
-import dayjs from 'dayjs';
-console.log(dayjs().format('HH:mm'));
+// JS
+import './modules/form/subtmit.js';
---
+++ src/modules/form/subtmit.js
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs';
+
+const form = document.querySelector('form');
+const selectedDate = document.getElementById('date');
+
+// Data atual para formatar o input
+
+const inputToday = dayjs(new Date()).format('YYYY-MM-DD');
+
+// Carrega a data atual e define a data minima como sendo a data atual
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
+
+form.onsubmit = (event) => {
+    // previne o comportamento padrao de carregar a pagina.
+    event.preventDefault();
+    console.log('enviado');
+};
